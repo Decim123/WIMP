0c1f8686af8e94e196d263f79183074e9ecf043d
Decim123 <aleksandrtuman@gmail.com>
2023-10-13 21:18:28 +0300

    main page was added

--- WIMP/WIMP/asgi.py
+++ WIMP/WIMP/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for wimp project.
+ASGI config for WIMP project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wimp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WIMP.settings')
 
 application = get_asgi_application()
--- WIMP/WIMP/wsgi.py
+++ WIMP/WIMP/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for wimp project.
+WSGI config for WIMP project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'wimp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WIMP.settings')
 
 application = get_wsgi_application()
